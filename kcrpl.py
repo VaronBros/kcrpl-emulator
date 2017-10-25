e86083c8ddce3cd5eb349d4bfcc37e8ede73382d
Varon.B <larsenawesome@gmail.com>
2017-10-25 10:47:23 -0500

    Updated Kcrpl talking machine
    made it more dank and fun

--- kcrpl.py
+++ kcrpl.py
@@ -4,7 +4,7 @@ import time
 
 print("KcrPL Emulator\n")
 
-tasks = ["Have any ideas why this guy is having issues?", "I'm gonna go take a shower.", "I'm going to sleep. Good night!", "Can I make a Wii emulator in Windows Batch?", "Make a support@riiconnect24.net email.", "Send me the new IOS patches please.", "I want to implement translation support in my batch script!", "Should I merge this PR?", "Help me I'm still getting 221001.", "I wanna be admin!", "Can you ban this spammer?", "Can I add more cities for Poland?"]
+tasks = ["Have any ideas why this guy is having issues?", "Don't talk about EVC, okay? It's almost done xDD.", "I'm going to sleep. Good night!", "Can I make a Wii emulator in Windows Batch?", "Make a support@riiconnect24.net email.", "Send me the new IOS patches please.", "I want to implement translation support in my batch script!", "Should I merge this PR?", "Help me I'm still getting 221001.", "I wanna be admin!", "Can you ban this spammer?", "Can I add more cities for Poland?"]
 tag_notag = ["@Larsenv ", ""]
 
 print("KcrPL: " + random.choice(tag_notag) + random.choice(tasks))
@@ -30,8 +30,8 @@ print("KcrPL: " + tag)
 
 if "/" in tag:
     time.sleep(1)
-    if tag == "/kcrplobjection" or tag == "/kcrplhmm" or tag == "/kcrplevidence": print("Spectra: <Phoenix Wright GIF>")
+    if tag == "/kcrplobjection" or tag == "/kcrplhmm" or tag == "/kcrplevidence": print("Spectra: <Harambe GIF>")
     elif tag == "/kcrplhaha": print("Spectra: <GIF>")
-    else: print("Spectra: <Destiny GIF>")
+    else: print("Spectra: <Harambe <GIF>")
 
 time.sleep(1)
